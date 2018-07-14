ce55e2e6e718fb65b364db43bb248b8761ac5bab
hpsanampudi <hpsanampudi@gmail.com>
2018-07-14 09:43:19 +0100

    Added T4 Templates
    Added T4 Templates

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ducksoft.SOA.DAL.Templates")]
+[assembly: AssemblyDescription("WCF T4 Templates DAL used across SOA application.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Duck Software Limited")]
+[assembly: AssemblyProduct("Ducksoft.SOA.DAL.Templates")]
+[assembly: AssemblyCopyright("Copyright © Duck Software Limited 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+[assembly: Guid("900e2928-9f0f-4f88-a1b3-05045e18e71c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version - Year
+//      Build Number - MonthDate
+//      Revision - HoursMinutes
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Ducksoft.SOA.DAL.Templates
+- WCF T4 Templates for DAL used across SOA application.
+- Nuget Package url: https://www.nuget.org/packages/Ducksoft.SOA.DAL.Templates/
